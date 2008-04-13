b7139b2e4373e1f47c01cf897a1a62c4020587f2
Balint Erdi <erdibalint@freemail.hu>
2008-04-13 09:00:44 +0200

    aligning views better; nice form builder
    Signed-off-by: Balint Erdi <erdibalint@freemail.hu>

--- app/helpers/nice_form_builder_helper.rb
+++ app/helpers/nice_form_builder_helper.rb
@@ -4,22 +4,23 @@ module NiceFormBuilder
     (field_helpers - %w(check_box radio_button hidden_field)).each do |selector|
       src = <<-END_SRC
       def #{selector}(field, options = {})
-        @template.content_tag("tr",
-                              @template.content_tag("td", field.to_s.humanize + ":") +
-                              @template.content_tag("td", super))
+        @template.content_tag("li",
+                              @template.content_tag("label", field.to_s.humanize + ":") +
+                              super
       end
       END_SRC
       class_eval src, __FILE__, __LINE__
     end
   end
 
-  def tabular_form_for(name, object = nil, options = nil, &proc)
-    concat("<table>", proc.binding)
+  def nice_form_for(name, object = nil, options = nil, &proc)
+    concat("<fieldset>", proc.binding)
+    concat("<legend> This is the legend </legend>", proc.binding)
     form_for(name,
              object,
              (options||{}).merge(:builder => NiceFormBuilder),
              &proc)
-    concat("</table>", proc.binding)
+    concat("</fieldset>", proc.binding)
   end
 
 end
