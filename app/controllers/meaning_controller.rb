1cee683bad41a768d6bd3734b1a53e4493b183ea
Balint Erdi <erdibalint@freemail.hu>
2008-04-13 16:52:57 +0200

    upgrading to rails 2.0.2 \!
    Signed-off-by: Balint Erdi <erdibalint@freemail.hu>

--- app/controllers/meaning_controller.rb
+++ app/controllers/meaning_controller.rb
@@ -1,6 +1,4 @@
 class MeaningController < ApplicationController
-  scaffold :meaning
-
   def new
     @msg = "Hello world 2"
   end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
   layout "standard-layout"
-  auto_complete_for :word, :name
+  # this somehow broke when I upgraded to Rails 2.0.2
+  # it is taken from Rails Recipes (Live Search), btw
+  # auto_complete_for :word, :name
 end
